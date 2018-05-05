489e8fe4d42ce3b61ed3a65745bd4d86bd31f9d3
Ofir Cohen <ofir.o.cohen@intel.com>
2018-05-05 16:10:13 +0300

    Added environment wrappers for VS and CLI

---
+++ open_cmd.bat
@@ -0,0 +1,5 @@
+set PATH=C:\face_swap\opencv-3.4.0\opencv-3.4.0\build\bin\Release;C:\qt_\5.6.0\msvc2015_64\bin;c:\face_swap\glew\bin\Release\x64;C:\Users\%USERNAME%\.caffe\dependencies\libraries_v140_x64_py27_1.1.0\libraries\bin;C:\Program Files (x86)\common files\intel\Shared Libraries\redist\intel64_win\compiler;C:\Users\%USERNAME%\.caffe\dependencies\libraries_v140_x64_py27_1.1.0\libraries\x64\vc14\bin;C:\Users\%USERNAME%\.caffe\dependencies\libraries_v140_x64_py27_1.1.0\libraries\lib;C:\face_swap\find_face_landmarks\build\install\bin;%PATH%
+		
+set Qt5Widgets_DIR=C:\QT_\5.6.0\msvc2015_64
+
+set QT_QPA_PLATFORM_PLUGIN_PATH=C:\QT_\5.6.0\msvc2015_64\plugins\platforms
